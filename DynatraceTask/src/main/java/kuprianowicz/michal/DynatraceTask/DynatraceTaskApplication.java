8b61d48d61a0b7eef48b79b2046a6a3c4206d1a6
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-04-23 14:26:09 +0200

    Template upload

---
+++ DynatraceTask/src/main/java/kuprianowicz/michal/DynatraceTask/DynatraceTaskApplication.java
@@ -0,0 +1,13 @@
+package kuprianowicz.michal.DynatraceTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynatraceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynatraceTaskApplication.class, args);
+	}
+
+}
---
+++ DynatraceTask/src/test/java/kuprianowicz/michal/DynatraceTask/DynatraceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kuprianowicz.michal.DynatraceTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynatraceTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
